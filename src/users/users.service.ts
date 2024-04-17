3c43260744ebe9b599a368a5a939651277e22b05
Andrey Emelianov <621077@mail.ru>
2024-04-17 17:50:32 +0300

    описал модели BlockList и BlockItem, сделал связи, создал контроллер, сервис и модуль для них, описал dto,  реализовал методы создания bockitem, полечения списка blockitems, удаление blockitem из списка, сделал миграцию призмы

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { AccountService } from 'src/account/account.service';
+import { BlockListService } from 'src/block-list/block-list.service';
 import { DbService } from 'src/db/db.service';
 
 @Injectable()
@@ -7,6 +8,7 @@ export class UsersService {
   constructor(
     private db: DbService,
     private accountService: AccountService,
+    private blockListService: BlockListService,
   ) {}
 
   async findByEmail(email: string) {
@@ -17,6 +19,7 @@ export class UsersService {
     const user = await this.db.user.create({ data: { email, hash, salt } });
 
     await this.accountService.createAccount(user.id);
+    await this.blockListService.create(user.id);
 
     return user;
   }
