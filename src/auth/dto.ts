a91d6722826c7b2c6eeed2151fed8636e4e06d55
Andrey Emelianov <621077@mail.ru>
2024-04-17 15:57:09 +0300

    подключил class-validator и transformer, добавил валидацию на логин и пароль

--- src/auth/dto.ts
+++ src/auth/dto.ts
@@ -1,18 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class SignUpBodyDto {
   @ApiProperty({ example: 'test@gmail.com' })
+  @IsEmail()
   email: string;
 
+  @IsNotEmpty()
   @ApiProperty({ example: '123456' })
   password: string;
 }
 
 export class SignInBodyDto {
   @ApiProperty({ example: 'test@gmail.com' })
+  @IsEmail()
   email: string;
 
   @ApiProperty({ example: '123456' })
+  @IsNotEmpty()
   password: string;
 }
 
